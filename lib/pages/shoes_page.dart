0e776f22599156191a7b5decf83e5987eed6fdcf
David Barcenas <daveepro@outlook.com>
2020-08-07 17:25:36 -0500

    feat(shoes): Create folder structure and add CustomAppBar

---
+++ lib/pages/shoes_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:shoes/widgets/custom_widgets.dart';
+
+class ShoesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: CustomAppBar(text: 'For you'));
+  }
+}
