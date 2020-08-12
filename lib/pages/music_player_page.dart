67285fdf9514286b18e8636ba5925954cc662fd7
David Barcenas <daveepro@outlook.com>
2020-08-11 19:54:41 -0500

    feat(player): Create disc image widget [LinearGradient, ClipRRect]

--- lib/pages/music_player_page.dart
+++ lib/pages/music_player_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shoes/widgets/player/custom_appbar.dart';
+import 'package:shoes/widgets/player/music_disc.dart';
 
 class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: [CustomAppBar()],
+      children: [CustomAppBar(), MusicDisc()],
     ));
   }
 }
